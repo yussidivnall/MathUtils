4cce4139bdb97133ad0773956da1b0808f78c509
uri <UC.dev.null@gmail.com>
2011-12-16 15:39:18 +0000

    Now generates a correct list of first 10,000 primes

--- numbertheory/primestat.c
+++ numbertheory/primestat.c
@@ -10,15 +10,34 @@ int firstprimes[10000];
 
 using namespace std;
 
+void dumpfirstprimes(){
+	for (int i=0;i<=10000;i++){
+		cout << firstprimes[i] << "\n";
+	}
+}
+
+
+int bruteisprime(int n){
+	if(n==2) return 1;
+	if(n%2==0)return 0; // divides by 2 (only 2 satisfy this as a prime)
+	for(int i=3;i<n;i+=2){ // We only need to increment by 2 if we're testing from 3, as all primes but 2 are odd
+		if(n%i == 0) return 0; //Not a prime
+	}
+	return 1;
+}
+
+
 void populatefirstprimes(){
 	firstprimes[0]=2;
 	firstprimes[1]=3;
 	int primeindex=2;
-	int test=4;
+	int test=5;
 	while (primeindex <=10000){
-		
-		test++;
-		primeindex++;
+		if(bruteisprime(test)){
+			firstprimes[primeindex]=test;
+			primeindex++;
+		}
+		test+=2;
 	}	
 }
 
@@ -28,34 +47,17 @@ void init(){
 	memset(bitbucket,false,sizeof(bitbucket));
 	memset(bitchance,0,sizeof(bitchance));
 	//Creates a list of first 10,000 primes - TO FINISH!
-	firstprimes[0]=2;
-	int lastprime=2;
-	bool isprime=true;
-	for(int primeindex=1;primeindex < 10000;primeindex++){
-		for(int testnum=lastprime+1;;testnum++){
-		isprime=true;
-		cout <<" testing "<< testnum << "\n";
-			for(int testprimeindex=0;testprimeindex <primeindex;testprimeindex++){
-				int testprime=firstprimes[testprimeindex];
-				cout << "does " << testnum << " divides by "<<testprime << "\n";
-				cout << testnum%testprime << "\n\n";
-				if( (testnum%testprime)==0){
-					isprime=false;
-					continue;
-				}
-			}
-			if(isprime){
-				firstprimes[primeindex]=testnum;
-				cout << "is "<<testnum << " prime? "<<"\n";
-			}
-		}	
-	}
+	populatefirstprimes();
 }
 
 
 int main(int argc,char ** argv){
 	init();
-	int somenumber=3;
+	dumpfirstprimes();
+	/*
+	int somenumber=23;
+	if(bruteisprime(somenumber)) cout << somenumber << " is prime \n";
+	else cout << somenumber << " isn't a prime \n";
 	bool *abit;
 	for (int i=sizeof(int)*4; i >=0;i--){
 		int bit= ((somenumber >> i)& 1);
@@ -63,6 +65,7 @@ int main(int argc,char ** argv){
 		cout << i << " " << bit <<"\n";
 		//cout << bit;
 	}
+	*/
 	cout << "\n";
 	//memcpy(abit,somenumber[2],1);
 	return 0;
