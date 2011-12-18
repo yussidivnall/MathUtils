a94761ab86bc41378fad6ccfbd7efdc965ab962d
uri <UC.dev.null@gmail.com>
2011-12-18 17:20:07 +0000

    some more fixing of stats

--- numbertheory/primestat.c
+++ numbertheory/primestat.c
@@ -5,7 +5,7 @@
 #include <iomanip>
 #include <gmpxx.h>
 #include <math.h>
-
+#include <limits>
 bool bitbucket[64];
 int bitchance[64];
 int firstprimes[10000];
@@ -29,7 +29,7 @@ int bruteisprime(int n){
 	
 	return 1;
 }
-int isprime(int n){
+int isprime(unsigned long long int n){
 	//First test that n%firstprimes[i]!=0
 	//for(int i=0;i<10000;i++){
 	//	if(n%firstprimes[i]==0) return 0; //Not a prime
@@ -37,7 +37,7 @@ int isprime(int n){
 	//Then preform thermat's little test
 	//is p is prime then, given a positive integer a
 	//a^p equiv a (mod p)
-	long int pwr=pow(2,n);
+	unsigned long long int pwr=pow(2,n);
 	//cout << "PWR:" <<pwr <<"\n";
 
 	if ( (pwr-2) % n == 0){
@@ -59,7 +59,25 @@ void populatefirstprimes(){
 		test+=2;
 	}	
 }
-
+void updatestats(unsigned long long int n){
+	//playground...
+	/*
+	cout << "=============\n";
+	cout << "n=" <<n << "\n";
+	bool res= (n & 1);
+	cout << res << "\n";
+	*/
+	for(int i=0;i<64;i++){
+		long long int pwr=pow(2,i);
+		bool bit= pwr & n;
+		cout << bit << " ";
+		if (bit){
+			//cout << "bit number " << i << "Is true \n";
+			bitchance[i]++;	
+		}
+	}
+	cout << "\n";
+}
 
 void init(){
 	memset(firstprimes,0,sizeof(firstprimes));
@@ -68,18 +86,43 @@ void init(){
 	//Creates a list of first 10,000 primes - TO FINISH!
 	populatefirstprimes();
 }
+void dumpchance(){
+	for(int i=63; i>=0 ;i--){
+		cout << bitchance[i] << " ";
+	}
+	cout <<"\n";
+}
 
 
 int main(int argc,char ** argv){
 	init();
 	//isprime(9);
+	dumpchance();
+	//updatestats(4);
+	dumpchance();
+	//cout << bitchance;	
 	
-	for(int i=2;i<=100;i++){
+	//Note to whoever is looking at this:
+	//I don't need to check every number, needs to be something like:
+	//long long int i=1023;i>=2;i -=2
+	//Just left it like this for the time being for readability
+	//ref:http://stackoverflow.com/questions/1855459/maximum-value-of-int
+	unsigned long long int mx=numeric_limits<unsigned long long int>::max();
+	//cout <<mx <<"\n";
+	//updatestats(18446744073709551615);
+	//cout <<" dumping \n";
+	//dumpchance();
+	
+	for(unsigned long long int i= mx;i >=2;i-=2){
 		if(isprime(i)){
 			cout << i << " is a prime \n";
+			updatestats(i);
+			dumpchance();
 		}
-		else cout << i << " is not a prime \n";
+		//else cout << i << " is not a prime \n";
+		
 	}
+	
 	cout << "\n";
 	return 0;
 }
